a2b8c1120cdf69258fbc01fab58ff2a27aa696ec
danikereselidze1 <dkere032@gmail.com>
2024-05-20 23:38:02 -0700

    level3

---
+++ LEVEL 3/homework/level3.py
@@ -0,0 +1,33 @@
+name="dani"
+surname="kereselidze"
+age=13
+height=1.60
+name="dani"
+surname="kereselidze"
+age=13
+height=1.60
+hobbie="basketball"
+location="Tbilis,Georgia"
+
+    
+    
+print("my name is" " " + name+ " " + surname+" " +str(age)+" years old " "and my height is "+str(height))
+location="Georgia,Tbilisi"
+hobbie="basketball"
+print("my hobbie is "+hobbie)
+print("I live in " +location )
+
+
+
+
+
+
+print(120 / 2)
+print(240 / 4)
+print(26*47)
+print(845*376)
+print(97658844*59779)
+print(567-437)
+print(8646+459)
+print(3568-143)
+print(3636565*74376237-5)
