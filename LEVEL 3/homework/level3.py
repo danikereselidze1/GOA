ac044e7526a0ead003fbe5e68868d937b572490e
danikereselidze1 <dkere032@gmail.com>
2024-05-21 19:41:49 -0700

    level3

--- LEVEL 3/homework/level3.py
+++ LEVEL 3/homework/level3.py
@@ -1,7 +1,6 @@
-name="dani"
+
 surname="kereselidze"
-age=13
-height=1.60
+
 name="dani"
 surname="kereselidze"
 age=13
@@ -24,10 +23,49 @@ print("I live in " +location )
 
 print(120 / 2)
 print(240 / 4)
+print(235/67)
+print(8778654//7565)
+print(978/4)
+print(972/42)
+print(689//73)
+print(237//9)
+print(90/2)
+print(34//2)
+print(357/23)
+
 print(26*47)
 print(845*376)
+print(4624*997)
+print(957*880)
+print(558* 807)
 print(97658844*59779)
-print(567-437)
-print(8646+459)
+print(4858*9659)
+print(3690*65)
+print(3957*98776)
+print(358*68)
+print(636*36)
+
+print(3568-1789)
 print(3568-143)
-print(3636565*74376237-5)
+print(689-353)
+print(4657-26)
+print(343-234)
+print(9345-946)
+print(725825483858453464-999999999)
+print(3458943-3884)
+print(9538-469)
+print(9659-849)
+print(4786-7858)
+
+print(9468+7+676)
+print(38569+0)
+print(68589+858)
+print(688484+4856)
+print(9863858+340589)
+print(6736509+89609)
+print(76947+9095)
+print(958+84)
+print(56857+859)
+print(859+959)
+
+
